ffe64f06307496c057d0cf5095087f12bf3a93ee
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-04-15 13:06:39 +0000

    refactor: og-image

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,29 +1,24 @@
 import { getCollection } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
 
-// Get all entries from the `docs` content collection.
 const entries = await getCollection("docs");
 
-// Map the entry array to an object with the page ID as key and the
-// frontmatter data as value.
 const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
-console.log("lool");
+
 export const { getStaticPaths, GET } = OGImageRoute({
-  // Pass down the documentation pages.
   pages,
-  // Define the name of the parameter used in the endpoint path, here `slug`
-  // as the file is named `[...slug].ts`.
   param: "slug",
-  // Define a function called for each page to customize the generated image.
-  getImageOptions: (_id, page: (typeof pages)[number]) => {
+  getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      // Use the page title and description as the image title and description.
       title: page.data.title,
       description: page.data.description,
-      // Customize various colors and add a border.
+      border: { width: 32, side: "inline-start" },
+      padding: 80,
       bgGradient: [[24, 24, 27]],
-      border: { color: [63, 63, 70], width: 20 },
-      padding: 120,
+      //TODO: add a background image
+      // bgImage: {
+      //   path: "./src/pages/og/_background-image.webp",
+      // },
     };
   },
 });
